01b47259812947369d9a0a2cfb2837241a87c539
taikomegane <taiko.megane.gobou@gmail.com>
2017-08-20 17:04:41 +0900

    change directory

---
+++ src/adapter_class/Main.java
@@ -0,0 +1,12 @@
+package adapter_class;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Print p = new PrintBanner("Hello");
+		p.printWeak();
+		p.printStrong();
+
+	}
+
+}
---
+++ src/adapter_class/Print.java
@@ -0,0 +1,8 @@
+package adapter_class;
+
+public interface Print {
+
+	public abstract void printWeak();
+
+	public abstract void printStrong();
+}
